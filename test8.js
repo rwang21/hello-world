be570ab6d2a6dbe1cffce0b944211dfa0cd1eeb2
rwang21 <Rong.Wang@ssa.gov>
2016-09-07 13:28:00 -0400

    new files

--- test8.js
+++ test8.js
@@ -1,4 +1,12 @@
+
+//read a file that is a list of words
+//split the string by line into an array
+//sorts the words alphabetically
+//write the sorted wordss to another file
+
 var fs = require('fs');
+var os = require("os");   
+var crlf = os.EOL;
 
 fs.readFile('file.txt', 'utf8', function(err, contents) {
     if (err) {
@@ -6,8 +14,33 @@ fs.readFile('file.txt', 'utf8', function(err, contents) {
         return;
     }
 
-    console.log(contents);
+    var arr = contents.split(crlf);
 
-});
+    arr.sort(function (a,b) {
+         if (typeof (a) === 'strig' && typeof(b) === 'string') {
+             s = a.toLowerCase();
+             b = b.toLowerCase();
+         }
 
-console.log('after calling readfile');
+         if (a > b) {
+             return 1;
+         }
+
+          if (a < b) {
+             return -1;
+         }
+
+         if (a === b) {
+             return 0;
+         }
+    })
+   fs.writeFile("./output.txt", arr.join(crlf), function(err) {
+    if(err) {
+            console.log(err);
+    } 
+    else {
+        console.log("Output saved to /output.txt.");
+    }
+    });
+
+})
