45561ad98d2d146514eb1fe6299e2ee7b424488f
rwang21 <Rong.Wang@ssa.gov>
2016-09-09 12:50:08 -0400

    add promise example

---
+++ app.js
@@ -0,0 +1,114 @@
+var express = require('express'),
+    app = express(),
+
+    dbInsertUser = require('./db.js').insertUser,
+    dbInsertTweet = require('./db.js').insertTweet,
+    dbGetTweets = require('./db.js').getTweets,
+    fs = require('fs'),
+    html = fs.readFileSync('./index.html'),
+    dbGetPassword = require('./db.js').getPassword,
+    bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post('/mainApp', function(req, res) {
+    var login = {
+        userid: req.body.userid,
+        password: req.body.password
+    };
+    console.log(login.password);
+    var p = dbGetPassword(login);
+    p.then(
+        (val) => {
+            console.log(val);
+            console.log(login.password);
+            if (login.password == val) {
+                res.send('Login Succesful');
+            } else {
+                res.send('Username or password issue!');
+            }
+        }
+    ).catch(
+        (err) => {
+            console.log(err);
+            res.send(err);
+        }
+    )
+});
+
+app.get('/', function(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.end(html);
+});
+
+app.get('/mainApp', function(req, res) {
+    res.send('logged in');
+})
+
+app.get('/adduser', function(req, res) {
+
+    var user = {
+        userid: req.query.userid,
+        password: req.query.password,
+        firstname: req.query.firstname,
+        lastname: req.query.lastname,
+        email: req.query.email
+
+    };
+    var p = dbInsertUser(user);
+    p.then(
+        (val) => {
+            res.send('User Added');
+        }
+    ).catch(
+        (err) => {
+            res.send(err);
+        }
+    )
+});
+
+app.get('/addtweet', function(req, res) {
+    var tweet = {
+        userid: req.query.userid,
+        tweetcontent: req.query.tweetcontent,
+        tweetts: new Date()
+    };
+
+    var p = dbInsertTweet(tweet);
+    p.then(
+        (val) => {
+            res.send('Tweet Added');
+        }
+    ).catch(
+        (err) => {
+            res.send(err);
+        }
+    )
+});
+
+app.get('/gettweets', function(req, res) {
+    var user = {
+        userid: req.query.userid,
+    };
+
+    var p = dbGetTweets(user);
+    p.then(
+        (val) => {
+            res.send(val);
+        }
+    ).catch(
+        (err) => {
+            res.send(err);
+        }
+    )
+});
+
+app.listen(8080, function() {
+    console.log('Example app listening on port 8080!');
+});
